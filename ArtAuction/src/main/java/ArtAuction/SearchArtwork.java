c55b0b441f2d3be3c49d9d8d08720f276482997a
Daphne Dao <daphnedao19@gmail.com>
2024-12-02 22:26:51 -0800

    fixed pfp in search jsp

--- ArtAuction/src/main/java/ArtAuction/SearchArtwork.java
+++ ArtAuction/src/main/java/ArtAuction/SearchArtwork.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SearchArtwork extends HttpServlet {
@@ -24,6 +25,30 @@ public class SearchArtwork extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+    	UserProfileDAO userDAO = new UserProfileDAO();
+    	ImageDAO imageDAO = new ImageDAO();
+    	Image image = null;
+        int userID = -1;
+    	
+    	int imageID = -1;
+    	
+    	// get logged in user id from the current session
+    	if (request.getSession().getAttribute("userID") != null) {
+    		userID = (int) request.getSession().getAttribute("userID");
+        	
+        	try {
+    			imageID = userDAO.getProfilePictureID(userID);
+    			//System.out.println(imageID);
+    			image = imageDAO.findImgByID(imageID);
+    			//System.out.println(image.getFilename());
+    		} catch (SQLException e) {
+    			// Auto-generated catch block
+    			e.printStackTrace();
+    		}
+    	}
+       // TODO When not logged in prompt user to login to see featured artworks based on their followings.
+    	
+    	request.setAttribute("image", image);
 
         String searchText = request.getParameter("searchText");
         
