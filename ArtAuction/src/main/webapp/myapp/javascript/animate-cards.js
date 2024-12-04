3eeef6a27fde83d4b218e4047a3b19d72982f450
Daphne Dao <daphnedao19@gmail.com>
2024-12-03 22:04:52 -0800

    fix homepage pfp

--- ArtAuction/src/main/webapp/myapp/javascript/animate-cards.js
+++ ArtAuction/src/main/webapp/myapp/javascript/animate-cards.js
@@ -3,37 +3,38 @@ const cards = document.querySelectorAll('.animated-card');
 cards.forEach(card => {
     let bounds;
 
-    function rotateToMouse(e) {
-        const mouseX = e.clientX;
-        const mouseY = e.clientY;
-        const leftX = mouseX - bounds.x;
-        const topY = mouseY - bounds.y;
-        const center = {
-            x: leftX - bounds.width / 2,
-            y: topY - bounds.height / 2
-        }
-        const distance = Math.sqrt(center.x**2 + center.y**2);
+	function rotateToMouse(e) {
+	        const mouseX = e.clientX;
+	        const mouseY = e.clientY;
+	        const leftX = mouseX - bounds.x;
+	        const topY = mouseY - bounds.y;
+	        const center = {
+	            x: leftX - bounds.width / 2,
+	            y: topY - bounds.height / 2
+	        }
+	        const distance = Math.sqrt(center.Math.pow(x,2) + center.Math.pow(y,2));
 
-        card.style.transform = `
-    scale3d(1.05, 1.05, 1.05)
-    rotate3d(
-      ${center.y / 100},
-      ${-center.x / 100},
-      0,
-      ${Math.log(distance)* 4}deg
-    )
-  `;
+			card.style.transform = `
+			    scale3d(1.05, 1.05, 1.05)
+			    rotate3d(
+			        ${center.y / 100},
+			        ${-(center.x / 100)},
+			        0,
+			        ${Math.log(distance) * 4}deg
+			    )
+			`;
 
-       card.querySelector('.glow').style.backgroundImage = `
-    radial-gradient(
-      circle at
-      ${center.x * 2 + bounds.width/2}px
-      ${center.y * 2 + bounds.height/2}px,
-      #ffffff55,
-      #0000000f
-    )
-  `;
-    }
+
+  card.querySelector('.glow').style.backgroundImage = `
+      radial-gradient(
+        circle at
+        ${center.x * 2 + bounds.width/2}px
+        ${center.y * 2 + bounds.height/2}px,
+        #ffffff55,
+        #0000000f
+      )
+    `;
+      }
 
     card.addEventListener('mouseenter', () => {
         bounds = card.getBoundingClientRect();
